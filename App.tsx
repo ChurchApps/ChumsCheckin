b1c2ee3ed8983fc329e7d98223b35ee4326a750f
Jeremy Zongker <jeremy@zongker.net>
2023-04-25 09:26:53 -0500

    Added eslint rules

--- App.tsx
+++ App.tsx
@@ -1,16 +1,16 @@
-import React, { useEffect } from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import { CheckinComplete } from './src/screens/CheckinComplete'
-import { Splash, Lookup, Services, Login, Household, SelectGroup, AddGuest, ScreenList, SelectChurch } from './src/screens'
-import { EnvironmentHelper } from './src/helpers'
-import { Printers } from './src/screens/Printers'
-import CodePush from 'react-native-code-push';
-import { ErrorHelper } from './src/helpers/ErrorHelper'
+import React, { useEffect } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { CheckinComplete } from "./src/screens/CheckinComplete";
+import { Splash, Lookup, Services, Login, Household, SelectGroup, AddGuest, ScreenList, SelectChurch } from "./src/screens";
+import { EnvironmentHelper } from "./src/helpers";
+import { Printers } from "./src/screens/Printers";
+import CodePush from "react-native-code-push";
+import { ErrorHelper } from "./src/helpers/ErrorHelper";
 
 const CODE_PUSH_OPTIONS = {
   checkFrequency: CodePush.CheckFrequency.ON_APP_START
-}
+};
 
 EnvironmentHelper.init();
 const stack = createStackNavigator<ScreenList>();
@@ -33,26 +33,26 @@ const App = () => {
       case CodePush.SyncStatus.UPDATE_INSTALLED: console.log("UPDATE_INSTALLED"); break;
       case CodePush.SyncStatus.UP_TO_DATE: console.log("UP_TO_DATE"); break;
     }
-    console.log('Codepush sync status', status);
-  }
+    console.log("Codepush sync status", status);
+  };
 
   return (
     <NavigationContainer>
-      <stack.Navigator screenOptions={{ headerShown: false, animationEnabled: false }} >
-        <stack.Screen name='Splash' component={Splash} />
-        <stack.Screen name='Login' component={Login} />
-        <stack.Screen name='Services' component={Services} />
-        <stack.Screen name='Lookup' component={Lookup} />
-        <stack.Screen name='Household' component={Household} />
-        <stack.Screen name='AddGuest' component={AddGuest} />
-        <stack.Screen name='CheckinComplete' component={CheckinComplete} />
-        <stack.Screen name='SelectGroup' component={SelectGroup} />
+      <stack.Navigator screenOptions={{ headerShown: false, animationEnabled: false }}>
+        <stack.Screen name="Splash" component={Splash} />
+        <stack.Screen name="Login" component={Login} />
+        <stack.Screen name="Services" component={Services} />
+        <stack.Screen name="Lookup" component={Lookup} />
+        <stack.Screen name="Household" component={Household} />
+        <stack.Screen name="AddGuest" component={AddGuest} />
+        <stack.Screen name="CheckinComplete" component={CheckinComplete} />
+        <stack.Screen name="SelectGroup" component={SelectGroup} />
         <stack.Screen name="SelectChurch" component={SelectChurch} />
         <stack.Screen name="Printers" component={Printers} />
       </stack.Navigator>
     </NavigationContainer>
-  )
-}
+  );
+};
 
 export default CodePush(CODE_PUSH_OPTIONS)(App);
 //export default createNavigation
