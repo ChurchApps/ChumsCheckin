dbb0629fad414672be55cabb470a5dbaaa7cb75d
Jeremy Zongker <jeremy@zongker.net>
2023-11-06 08:08:01 -0600

    Updated version number for app store deploy

--- src/helpers/EnvironmentHelper.ts
+++ src/helpers/EnvironmentHelper.ts
@@ -8,7 +8,7 @@ export class EnvironmentHelper {
 
   static init = () => {
     let stage = STAGE;
-    //stage = "prod";
+    stage = "prod";
     switch (stage) {
       case "staging": EnvironmentHelper.initStaging(); break;
       case "prod": EnvironmentHelper.initProd(); break;
@@ -18,28 +18,28 @@ export class EnvironmentHelper {
       { keyName: "MembershipApi", url: EnvironmentHelper.MembershipApi, jwt: "", permisssions: [] },
       { keyName: "AttendanceApi", url: EnvironmentHelper.AttendanceApi, jwt: "", permisssions: [] },
     ];
-  }
+  };
 
   static initDev = () => {
     this.initStaging();
     EnvironmentHelper.MembershipApi = MEMBERSHIP_API || EnvironmentHelper.MembershipApi;
     EnvironmentHelper.AttendanceApi = ATTENDANCE_API || EnvironmentHelper.AttendanceApi;
     EnvironmentHelper.ContentRoot = CONTENT_ROOT || EnvironmentHelper.ContentRoot;
-  }
+  };
 
   //NOTE: None of these values are secret.
   static initStaging = () => {
     EnvironmentHelper.MembershipApi = "https://membershipapi.staging.churchapps.org";
     EnvironmentHelper.AttendanceApi = "https://attendanceapi.staging.churchapps.org";
     EnvironmentHelper.ContentRoot = "https://content.staging.churchapps.org";
-  }
+  };
 
   //NOTE: None of these values are secret.
   static initProd = () => {
     EnvironmentHelper.MembershipApi = "https://membershipapi.churchapps.org";
     EnvironmentHelper.AttendanceApi = "https://attendanceapi.churchapps.org";
     EnvironmentHelper.ContentRoot = "https://content.churchapps.org";
-  }
+  };
 }
 
 
