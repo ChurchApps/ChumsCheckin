57c7fcabb64d4a055dd78d765dbdbe48338c85d5
Jeremy Zongker <jeremy@zongker.net>
2025-01-11 18:17:42 -0600

    Removed cxx folder

--- android/app/.cxx/Debug/1a554y1l/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/fbjni/fbjniConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET fbjni::fbjni)
-add_library(fbjni::fbjni SHARED IMPORTED)
-set_target_properties(fbjni::fbjni PROPERTIES
-    IMPORTED_LOCATION "/Users/vishalsharma/.gradle/caches/8.10.2/transforms/b379def3a3bfd59679a960d99f4a70ee/transformed/fbjni-0.6.0/prefab/modules/fbjni/libs/android.armeabi-v7a/libfbjni.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/Users/vishalsharma/.gradle/caches/8.10.2/transforms/b379def3a3bfd59679a960d99f4a70ee/transformed/fbjni-0.6.0/prefab/modules/fbjni/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
--- android/app/.cxx/Debug/1a554y1l/prefab/x86/prefab/lib/i686-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::libhermes)
-add_library(hermes-engine::libhermes SHARED IMPORTED)
-set_target_properties(hermes-engine::libhermes PROPERTIES
-    IMPORTED_LOCATION "/Users/vishalsharma/.gradle/caches/8.10.2/transforms/80348eae9836826b4026444bfa26e5ed/transformed/hermes-android-0.76.5-debug/prefab/modules/libhermes/libs/android.x86/libhermes.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/Users/vishalsharma/.gradle/caches/8.10.2/transforms/80348eae9836826b4026444bfa26e5ed/transformed/hermes-android-0.76.5-debug/prefab/modules/libhermes/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
